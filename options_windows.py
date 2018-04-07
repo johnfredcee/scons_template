4ebc7c066b8353c7fb2839fb9ba3848cf1c3ac3a
John Connors <johnfredcee@gmail.com>
2018-04-07 10:28:26 +0100

    Updated to work with Visual Studio on Windows

--- options_windows.py
+++ options_windows.py
@@ -1,10 +1,10 @@
 # These are the tools we pull from ./site_scons/site_tools for building the program on this platform
 #PROJECT_TOOLS=[ "vcbuildtool", "boost" ]
 # This is for the built in SCons Microsoft Visual Studio Support
-PROJECT_TOOLS = [ "msvs", "boost" ]
+PROJECT_TOOLS = [ "msvc", "mslink", "boost" ]
 VC_BUILD_TOOLS_PATH="c:/Program Files (x86)/Microsoft Visual C++ Build Tools"
-BOOST_INCLUDE_PATH="C:/wsr/local/libraries/boost/include"
-BOOST_LIB_PATH="C:/wsr/local/libraries/boost/lib/msw/x86"
+BOOST_INCLUDE_PATH="D:/Dev/libraries/boost_1_62_0"
+BOOST_LIB_PATH="D:/Dev/libraries/boost_1_62_0/stage/lib"
 BOOST_LIBRARIES="none"
 CINDER_INCLUDE_PATH="C:/wsr/local/libraries/cinder/include"
 CINDER_LIB_PATH="C:/wsr/local/libraries/cinder/lib/msw/x86"
